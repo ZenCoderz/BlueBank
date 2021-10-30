3280efe22bc9fadf56b6a4b53c2ef941f1de2f33
paulfms <pauldevop@gmail.com>
2021-10-29 22:48:52 -0300

    Initial Commit

---
+++ src/main/java/com/zencoderz/bluebank/BluebankApplication.java
@@ -0,0 +1,13 @@
+package com.zencoderz.bluebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluebankApplication.class, args);
+	}
+
+}
