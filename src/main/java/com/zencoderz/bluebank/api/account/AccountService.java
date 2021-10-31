ca653a5c1a036ad174c1f88ba22e93e116a6803f
ticoyk <didimoykym@gmail.com>
2021-10-31 12:14:06 -0300

    Created Account Service

---
+++ src/main/java/com/zencoderz/bluebank/api/account/AccountService.java
@@ -0,0 +1,18 @@
+package com.zencoderz.bluebank.api.account;
+
+
+import com.zencoderz.bluebank.api.account.dto.AccountDTO;
+import com.zencoderz.bluebank.api.account.dto.AccountFormCreateDTO;
+import com.zencoderz.bluebank.api.account.dto.AccountFormUpdateDTO;
+
+import java.util.Set;
+import java.util.UUID;
+
+public interface AccountService {
+
+    Set<AccountDTO> getAccountsDTO();
+    AccountDTO getAccountDTOById(UUID id);
+    AccountDTO createAccount(AccountFormCreateDTO accountFormCreateDTO);
+    AccountDTO updateAccount(UUID id, AccountFormUpdateDTO accountFormUpdateDTO);
+
+}
