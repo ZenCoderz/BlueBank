24ef3c79e43c5411d1ee67740e08649c08782fe3
ticoyk <didimoykym@gmail.com>
2021-10-31 12:13:24 -0300

    Created Account Repository

---
+++ src/main/java/com/zencoderz/bluebank/api/account/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.zencoderz.bluebank.api.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+}
