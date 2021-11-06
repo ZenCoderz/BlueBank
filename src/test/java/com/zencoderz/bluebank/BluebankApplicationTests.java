78711227332b49fbe7c15c917f0fb8a79d6afe06
ticoyk <didimoykym@gmail.com>
2021-11-06 13:43:38 -0300

    Changed local language to English

--- src/test/java/com/zencoderz/bluebank/BluebankApplicationTests.java
+++ src/test/java/com/zencoderz/bluebank/BluebankApplicationTests.java
@@ -1,14 +1,8 @@
 package com.zencoderz.bluebank;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class BluebankApplicationTests {
-
-	@Test
-	void contextLoads() {
-
-	}
 
 }
