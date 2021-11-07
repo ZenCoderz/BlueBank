60c3352f7370ad1e52359f87e316713eb070c09f
ticoyk <didimoykym@gmail.com>
2021-11-07 09:11:35 -0300

    Added default values for swagger documentation.

--- src/main/java/com/zencoderz/bluebank/api/account/dto/AccountFormCreateDTO.java
+++ src/main/java/com/zencoderz/bluebank/api/account/dto/AccountFormCreateDTO.java
@@ -1,5 +1,7 @@
 package com.zencoderz.bluebank.api.account.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,14 +14,18 @@ import javax.validation.constraints.Size;
 public class AccountFormCreateDTO {
 	
 	@Size(min=4, max=4)
+    @Schema(example = "4444")
     private String agency;
 
     @Size(min=1, max=10)
+    @Schema(example = "1010101010")
     private String accountNumber;
 
     @Size(min=1, max=1)
+    @Schema(example = "1")
     private String digit;
 
+    @Schema(example = "500")
     private Double credit = 0D;
 
 }
